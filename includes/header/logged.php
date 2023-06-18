667239d299465d3592bcac5de1c5c3ddd53e9b85
Kacper <kacperkapitrebacz@gmail.com>
2023-06-18 16:34:15 +0200

    Changes in file system and add header

---
+++ includes/header/logged.php
@@ -0,0 +1,34 @@
+<header class="p-3 mb-3 border-bottom">
+     <div class="container">
+          <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+               <a href="#" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none me-3">
+                    <img src="./../img/logo.png" alt="Logo" height="40px">
+               </a>
+
+               <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="./index.php" class="nav-link px-3 link-dark">HOME</a></li>
+                    <li><a href="./public/items/items.php" class="nav-link px-3 link-dark">ITEMS</a></li>
+                    <li><a href="./public/lists/create_list.php" class="nav-link px-3 link-dark">NEW SET</a></li>
+                    <li><a href="./public/about.php" class="nav-link px-3 link-dark">ABOUT</a></li>
+               </ul>
+
+               <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                    <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+               </form>
+
+               <div class="dropdown text-end">
+                    <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                         <img src="./../img/user_icon.jpg" alt="mdo" width="32" height="32" class="rounded-circle">
+                    </a>
+                    <ul class="dropdown-menu text-small">
+                         <li><a class="dropdown-item" href="#">My sets</a></li>
+                         <li><a class="dropdown-item" href="#">Profile</a></li>
+                         <li>
+                              <hr class="dropdown-divider">
+                         </li>
+                         <li><a class="dropdown-item" href="#">Sign out</a></li>
+                    </ul>
+               </div>
+          </div>
+     </div>
+</header>
---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Copeland - Drum sets configurator</title>
+     <link rel="stylesheet" href="./../css/main.css">
+</head>
+
+<body>
+
+     <?php
+          require './../includes/header/unlogged.php';
+     ?>
+
+     <script src="./../bootstrap/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
