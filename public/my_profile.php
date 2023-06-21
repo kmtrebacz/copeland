04ea3bfa66e9e2d078e6771f8a19a3385f2dbd22
Kacper <kacperkapitrebacz@gmail.com>
2023-06-21 19:02:05 +0200

    Add info in my_profile.php

--- public/my_profile.php
+++ public/my_profile.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION["userid"])) {
-    header("location: ./../index.php");
+     header("location: ./../index.php");
 }
 
 ?>
@@ -25,13 +25,32 @@ if (!isset($_SESSION["userid"])) {
 
      <main class="container py-3">
 
+          <?php
+          $name = $_SESSION["userid"];
+          $sql = "SELECT * FROM users WHERE username = '$name'";
+
+          require_once './../includes/db_connection.php';
+
+          $result = $conn->query($sql);
+
+          if ($result->num_rows > 0) {
+               $row = $result->fetch_assoc();
+               echo '<div class="col-4"><table class="table table-striped"><tbody><tr>';
+               echo '<th scope="row">Username</th><td>' . $row["username"] . '</td></tr>';
+               echo '<tr><th scope="row">Email</th><td>' . $row["email"] . '</td></tr>';
+               echo '</tbody></table></div>';
+          } else {
+               echo "No results found.";
+          }
+          ?>
+
      </main>
 
      <?php
      include_once './../includes/footer.html';
      ?>
 
-<?php include_once './../includes/bootstrap_js.html'; ?>
+     <?php include_once './../includes/bootstrap_js.html'; ?>
 </body>
 
 </html>
