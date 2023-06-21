8a611de122e7ae732cfd548be03a52fb0d0ee4d6
Kacper <kacperkapitrebacz@gmail.com>
2023-06-21 19:24:23 +0200

    Add blank new_item.php page

---
+++ public/new_item.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>NEW ITEM - Copeland - Drum sets configurator</title>
+     <?php include_once './../includes/bootstrap_css.html'; ?>
+</head>
+
+<body>
+
+     <?php
+     include_once './../includes/header/header_choosed.inc.php';
+     ?>
+
+     <main class="container py-3">
+
+     </main>
+
+     <?php
+     include_once './../includes/footer.html';
+     ?>
+
+     <?php include_once './../includes/bootstrap_js.html'; ?>
+</body>
+
+</html>
