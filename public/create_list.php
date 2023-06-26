1cf082122a94e71cf255a5d55143877d7f710705
Kacper <kacperkapitrebacz@gmail.com>
2023-06-26 21:58:55 +0200

    Creating new list works

--- public/create_list.php
+++ public/create_list.php
@@ -19,7 +19,29 @@ session_start();
      ?>
 
      <main class="container py-3">
+          <div class="col-10 col-lg-3 mx-auto">
+               <h3 class="text-center">NEW LIST</h3>
 
+               <?php
+               if (isset($_GET["error"])) {
+                    if ($_GET["error"] == "stmtfaild"){
+                         echo "<p><strong>Somethink went wrong, try again!</strong></p>";
+                    }
+                    else if ($_GET["error"] == "none"){
+                         echo "<p><strong>New list have been created!</strong></p>";
+                    }
+               }
+               ?>
+
+               <form action="./php_inc/new_list.inc.php" method="POST">
+                    <input type="text" name="list_name" id="list_name" required class="form-control my-2" placeholder="List name">
+
+                    <input type="checkbox" role="switch" id="list_public" name="list_public">
+                    <label for="list_public">Public</label>
+
+                    <button class="w-100 btn btn-lg btn-primary my-2" type="submit" name="submit">Sign in</button>
+               </form>
+          </div>
      </main>
 
      <?php
---
+++ public/php_inc/new_list.inc.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+require_once './../../includes/db_connection.php';
+
+$list_name = $_POST['list_name'];
+$list_public = $_POST['list_public'];
+$list_public_checked = 0;
+$user_id = $_SESSION['userid'];
+
+if (isset($_POST['list_public']) && $_POST['list_public'] == 'on') {
+    $list_public_checked = 1;
+}
+
+$sql = "SELECT user_id FROM users WHERE username = '$user_id'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $user_id = $row["user_id"];
+}
+
+$sql = "INSERT INTO `lists`(`user_id`, `list_name`, `is_public`) VALUES ('$user_id','$list_name','$list_public_checked')";
+
+$stmt = $conn->prepare($sql);
+
+if (!$stmt) {
+    die("Error preparing statement: " . $conn->error);
+}
+
+if ($stmt->execute()) {
+    header("location: ./../create_list.php?error=none");
+} else {
+    header("location: ./../create_list.php?error=stmtfailed");
+}
+
+$stmt->close();
+$conn->close();
+
+exit();
