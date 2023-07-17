a05cc5d994c499a27af15f874f8885c10be384c4
Kacper <kacperkapitrebacz@gmail.com>
2023-07-18 00:17:59 +0200

    items.php shows all of available items

--- public/items.php
+++ public/items.php
@@ -20,13 +20,41 @@ session_start();
 
      <main class="container py-3">
 
+          <?php
+          require('./../includes/db_connection.php');
+
+          $query = "SELECT items.item_name, categories.category_name, items.size FROM items JOIN categories ON categories.category_id = items.category_id WHERE 1;";
+          $result = $conn->query($query);
+
+          if ($result->num_rows > 0) {
+               echo '<div class="row">';
+               while ($row = $result->fetch_assoc()) {
+                    echo '<div class="px-2 col-10 col-lg-4 mx-auto mx-lg-0 my-2">
+                    <div class="card">
+                    <div class="card-body">
+                    <h4 class="m-0" class="card-title">' . $row['item_name'] . '</h4>
+                    <p class="my-2" style="font-size: 15px;">CATEGORY: ' . $row['category_name'] . '</p>
+                    <p class="my-2" style="font-size: 15px;">SIZE: ' . $row['size'] . '</p>
+                    </div>
+                    </div>
+                    </div>';
+               }
+               echo '</div>';
+          } else {
+               echo "<p><strong>There are no items</strong></p>";
+          }
+
+          $conn->close();
+          ?>
+
      </main>
+
 
      <?php
      include_once './../includes/footer.html';
      ?>
 
-<?php include_once './../includes/bootstrap_js.html'; ?>
+     <?php include_once './../includes/bootstrap_js.html'; ?>
 </body>
 
 </html>
