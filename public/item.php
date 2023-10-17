c3b1f6849633b66cd5caed71a9a09519455b40f7
Kacper <kacperkapitrebacz@gmail.com>
2023-10-17 20:17:02 +0200

    FEAT: Add twig render to item.php

--- public/item.php
+++ public/item.php
@@ -1,108 +1,42 @@
 <?php
-session_start();
-?>
+require_once './../vendor/autoload.php';
+require_once './php_inc/db_functions.inc.php';
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>ITEM - Copeland - Drum sets configurator</title>
-     <?php include_once './../includes/bootstrap_css.html'; ?>
-</head>
+$conn = dbConnect();
 
-<body>
-
-     <?php
-     include_once './../includes/header/header_choosed.inc.php';
-     ?>
-
-     <main class="container py-3">
-
-          <?php
-          require('./php_inc/db_functions.inc.php');
-
-          $conn = dbConnect();
-
-          if ($_SERVER["REQUEST_METHOD"] == "GET") {
-               if (isset($_SESSION["userid"])) {
-
-                    $resultLists = dbQuery($conn, "SELECT lists.list_id, lists.list_name FROM lists JOIN users ON users.user_id = lists.user_id WHERE users.username='" . $_SESSION['userid'] . "';");
-                    $lists = '';
-                    if ($resultLists->num_rows > 0) {
-                         while ($row = $resultLists->fetch_assoc()) {
-                              $lists .= '<input type="checkbox" name="item_name_' . $row['list_name'] . '" value="' . $row['list_id'] . '">  <label>' . $row['list_name'] . '</label><br>';
-                         }
-                         $lists .= '<input type="submit" class="mt-2 btn btn-primary" value="Add to list">';
-                    }
-
-                    echo '<section class="py-5">
-                    <div class="container px-4 px-lg-5 my-5">
-                    <div class="row gx-4 gx-lg-5 align-items-center">
-                    <div class="col-md-6"></div>
-                    <div class="col-md-6">
-                    <h1 class="display-5 fw-bolder">' . $_GET['item_name'] . '</h1>
-                    <h3>' . $_GET['category_name'] . '</h3>
-                    <p>Size: ' . $_GET['size'] . '</p>
-                    <div class="d-flex">
-                    <div>
-                    <div class="position-absolute p-3 border rounded" style="display: none; background: #fff; margin-top: 46px; z-index: 111;">
-                    <form method="get" action="./php_inc/add_item_to_list.inc.php"><input type="text" name="item_id" value="' . $_GET['item_id'] . '" class="d-none">
-                    ' . $lists . ' </form>                   
-                    </div>
-                    <div class="popover" data-on="0">
-                    <button type="button" class="btn btn-primary">+ Add to list</button>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </section>';
-               } else {
-                    echo '<section class="py-5">
-                    <div class="container px-4 px-lg-5 my-5">
-                    <div class="row gx-4 gx-lg-5 align-items-center">
-                    <div class="col-md-6"></div>
-                    <div class="col-md-6">
-                    <h1 class="display-5 fw-bolder">' . $_GET['item_name'] . '</h1>
-                    <h3>' . $_GET['category_name'] . '</h3>
-                    <p>Size: ' . $_GET['size'] . '</p>
-                    <div class="d-flex">
-                    <div>
-                    <div class="position-absolute p-3 border rounded" style="display: none; background: #fff; margin-top: 46px; z-index: 111;">
-                    <strong>You are not logged in</strong>
-                    </div>
-                    <div class="popover" data-on="0">
-                    <button type="button" class="btn btn-primary">+ Add to list</button>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </section>';
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+     if (isset($_SESSION["userid"])) {
+          $resultLists = dbQuery($conn, "SELECT lists.list_id, lists.list_name FROM lists JOIN users ON users.user_id = lists.user_id WHERE users.username='" . $_SESSION['userid'] . "';");
+          $lists = '';
+          if ($resultLists->num_rows > 0) {
+               while ($row = $resultLists->fetch_assoc()) {
+                    $lists .= '<input type="checkbox" name="item_name_' . $row['list_name'] . '" value="' . $row['list_id'] . '">  <label>' . $row['list_name'] . '</label><br>';
                }
-
-               $view_count_added = (int)$_GET['view_count'] + 1;
-
-               $sqlViewCount = 'UPDATE items SET items.items_view_count="' . $view_count_added . '" WHERE items.item_name = "' . $_GET['item_name'] . '"  AND items.size = "' . $_GET['size'] . '"';
-
-               dbQuery($conn, $sqlViewCount);
-               
-               dbClose($conn);
+               $lists .= '<input type="submit" class="mt-2 btn btn-primary" value="Add to list">';
           }
-          ?>
+     }
 
-     </main>
 
-     <?php
-     include_once './../includes/footer.html';
-     ?>
+     $loader = new \Twig\Loader\FilesystemLoader('./../templates/');
+     $twig = new \Twig\Environment($loader, [
+          'cache' => './../cache',
+     ]);
+     $template = $twig->load('item.twig');
 
-     <script src="./../js/popover.js"></script>
-     <?php include_once './../includes/bootstrap_js.html'; ?>
-</body>
+     echo $template->render([
+          'name' => $_GET['item_name'],
+          'category' => $_GET['category_name'],
+          'size' => $_GET['size'],
+          'view_count' => $_GET['view_count'],
+     ]);
 
-</html>
+     $view_count_added = (int)$_GET['view_count'] + 1;
+
+     $sqlViewCount = 'UPDATE items SET items.items_view_count="' . $view_count_added . '" WHERE items.item_name = "' . $_GET['item_name'] . '"  AND items.size = "' . $_GET['size'] . '"';
+
+     dbQuery($conn, $sqlViewCount);
+               
+     dbClose($conn);
+}
+?>
