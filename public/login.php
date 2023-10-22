05811eb1533e8b81ad54edaf642e0cfa994c0695
Kacper <kacperkapitrebacz@gmail.com>
2023-10-22 20:19:36 +0200

    FEAT: Add twig render to login.php

--- public/login.php
+++ public/login.php
@@ -1,53 +1,10 @@
 <?php
-session_start();
-?>
+require_once "./../vendor/autoload.php";
 
-<!DOCTYPE html>
-<html lang="en">
+$loader = new \Twig\Loader\FilesystemLoader("./../templates/");
+$twig = new \Twig\Environment($loader, [
+		"cache" => "./../cache",
+]);
+$template = $twig->load("login.twig");
 
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>LOGIN - Copeland - Drum sets configurator</title>
-	<?php include_once "./../includes/bootstrap_css.html"; ?>
-</head>
-
-<body>
-
-	<?php
-	include_once "./../includes/header/header_choosed.inc.php";
-	?>
-	<main class="container py-3">
-
-
-		<div class="col-10 col-lg-3 mx-auto">
-			<h3 class="text-center">LOGIN</h3>
-
-			<?php
-			if (isset($_GET["error"])) {
-				if ($_GET["error"] == "wronglogin") {
-					print("<p><strong>Incorrect login information!</strong></p>");
-				}
-			}
-			?>
-
-			<form action="./php_inc/login.inc.php" method="POST">
-				<input type="text" id="name" name="name" required class="form-control my-2" placeholder="Username">
-				
-				<input type="password" id="password" name="password" required class="form-control my-2" placeholder="Password">
-
-				<button class="w-100 btn btn-lg btn-primary my-2" type="submit" name="submit">Sign in</button>
-			</form>
-		</div>
-
-
-	</main>
-
-	<?php
-	include_once "./../includes/footer.html";
-	?>
-
-	<?php include_once "./../includes/bootstrap_js.html"; ?>
-</body>
-
-</html>
+print($template->render());
