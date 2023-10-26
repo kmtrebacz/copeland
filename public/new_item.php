d62a0373a58ece9230d91d22ed2283a506b6b7c9
Kacper <kacperkapitrebacz@gmail.com>
2023-10-26 19:32:19 +0200

    FEAT: Add twig render to new_item.php

--- public/new_item.php
+++ public/new_item.php
@@ -1,32 +1,8 @@
 <?php
-session_start();
-?>
+$loader = new \Twig\Loader\FilesystemLoader("./../templates/");
+$twig = new \Twig\Environment($loader, [
+	"cache" => "./../cache",
+]);
+$template = $twig->load("new_item.twig");
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>NEW ITEM - Copeland - Drum sets configurator</title>
-	<?php include_once "./../includes/bootstrap_css.html"; ?>
-</head>
-
-<body>
-
-	<?php
-	include_once "./../includes/header/header_choosed.inc.php";
-	?>
-
-	<main class="container py-3">
-
-	</main>
-
-	<?php
-	include_once "./../includes/footer.html";
-	?>
-
-	<?php include_once "./../includes/bootstrap_js.html"; ?>
-</body>
-
-</html>
+print($template->render());
