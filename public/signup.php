8c19a674156d6971f044c02d0dc21c6dbab86a24
Kacper <kacperkapitrebacz@gmail.com>
2023-10-28 22:06:33 +0200

    FEAT: Add twig render to signup.php

--- public/signup.php
+++ public/signup.php
@@ -1,73 +1,45 @@
 <?php
-session_start();
-?>
+if (!isset($_SESSION["userid"])) header("location: ./../index.php");
 
-<!DOCTYPE html>
-<html lang="en">
+require_once "./../vendor/autoload.php";
+require_once "./php_inc/db_functions.inc.php";
 
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>SIGN UP - Copeland - Drum sets configurator</title>
-	<?php include_once "./../includes/bootstrap_css.html"; ?>
-</head>
 
-<body>
+function errorHandler(){
+	if (isset($_GET["error"])) {
+		switch (isset($_GET["error"])) {
+			case $_GET["error"] == "invaliduid":
+				return "Choose a proper username!";
 
-	<?php
-	include_once "./../includes/header/header_choosed.inc.php";
-	?>
-	
-	<main class="container py-3">
-		<div class="col-10 col-lg-3 mx-auto">
-			<h3 class="text-center">SIGN UP</h3>
+			case $_GET["error"] == "invaliduid":
+				return "Choose a proper email!";
 
-			<?php
-			if (isset($_GET["error"])) {
-				if ($_GET["error"] == "invaliduid"){
-					print("<p><strong>Choose a proper username!</strong></p>");
-				}
-				else if ($_GET["error"] == "invalidemail"){
-					print("<p><strong>Choose a proper email!</strong></p>");
-				}
-				else if ($_GET["error"] == "passwordsdontmatch"){
-					print("<p><strong>Passwords doesn"t match!</strong></p>");
-				}
-				else if ($_GET["error"] == "usernametaken"){
-					print("<p><strong>This username is already associated with an existing account!</strong></p>");
-				} 
-				else if ($_GET["error"] == "emailwasused"){
-					print("<p><strong>This email address is already associated with an existing account!</strong></p>");
-				}
-				else if ($_GET["error"] == "stmtfaild"){
-					print("<p><strong>Somethink went wrong, try again!</strong></p>");
-				}
-				else if ($_GET["error"] == "none"){
-					print("<p><strong>You have signed up!</strong></p>");
-				}
-			}
-			?>
+			case $_GET["error"] == "passwordsdontmatch":
+				return "This username is already associated with an existing account!";
 
-			<form action="./php_inc/singup.inc.php" method="POST">
-				<input type="text" name="name" id="name" required class="form-control my-2" placeholder="Username">
-				
-				<input type="email" name="email" id="email" required class="form-control my-2" placeholder="Email">
+			case $_GET["error"] == "usernametaken":
+				return "Choose a proper username!";
 
-				<input type="password" name="pwd" id="pwd" required class="form-control my-2" placeholder="Password">
+			case $_GET["error"] == "emailwasused":
+				return "This email address is already associated with an existing account!";
 
-				<input type="password" name="r_pwd" id="r_pwd" required class="form-control my-2" placeholder="Repeat password">
+			case $_GET["error"] == "stmtfaild":
+				return "Somethink went wrong, try again!";
 
-				<button class="w-100 btn btn-lg btn-primary my-2" type="submit" name="submit">Sign in</button>
+			case $_GET["error"] == "none":
+				return "You have signed up!";
+		}
+	}
+}
 
-			</form>
-		</div>
-	</main>
+$loader = new \Twig\Loader\FilesystemLoader("./../templates/");
+$twig = new \Twig\Environment($loader, [
+	"cache" => "./../cache",
+]);
+$template = $twig->load("my_profile.twig");
 
-	<?php
-	include_once "./../includes/footer.html";
-	?>
+print($template->render([
+	"error" => errorHandler(),
+]));
 
-<?php include_once "./../includes/bootstrap_js.html"; ?>
-</body>
 
-</html>
