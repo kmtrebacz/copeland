7c86134c6ace3a933a88054045568d7a194059db
Kacper <kacperkapitrebacz@gmail.com>
2023-11-17 22:47:56 +0100

    FIX: Fix bug with displaying selected lists in add_item_to_list.php

--- public/include/add_item_to_list.inc.php
+++ public/include/add_item_to_list.inc.php
@@ -6,7 +6,7 @@ require_once "./db_connection.inc.php";
 $conn = dbConnect();
 
 if ($_SERVER["REQUEST_METHOD"] === "GET") {
-	foreach ($_POST as $paramName => $paramValue) {
+	foreach ($_GET as $paramName => $paramValue) {
 		if (strpos($paramName, "item_name_") === 0) {
 			$item = substr($paramName, strlen("item_name_"));
 
