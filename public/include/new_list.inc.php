2ae8a144cbc7d58471f936b52ff285e363ed4047
kacper <kacperkapitrebacz@gmail.com>
2023-11-25 00:30:19 +0100

    FIX: Fix an issue related to adding a new list

--- public/include/new_list.inc.php
+++ public/include/new_list.inc.php
@@ -5,8 +5,8 @@ require_once "./db_connection.inc.php";
 
 $db = dbConnect();
 
-$listName          = $_POST["list_name"];
-$listPublic        = $_POST["list_public"];
+$postListName      = $_POST["list_name"];
+$postListPublic    = $_POST["list_public"];
 $listPublicChecked = 0;
 $userId            = $_SESSION["userId"];
 
@@ -16,11 +16,9 @@ if (isset($_POST["list_public"]) && $_POST["list_public"] == "on") {
 
 $dbResult = dbQuery("SELECT user_id FROM users WHERE username = ?;", [$userId]);
 
-if (sizeof($dbResult) > 0) {
-	$userId = $row["user_id"];
-}
+$resultUserId = $dbResult["user_id"];
 
-$dbResult = dbQuery("INSERT INTO `lists`(`user_id`, `list_name`, `is_public`) VALUES (?, ?, ?);", [$user_id, $list_name, $list_public_checked]);
+$dbResult = dbQuery("INSERT INTO `lists`(`user_id`, `list_name`, `is_public`) VALUES (?, ?, ?);", [$resultUserId, $postListName, $listPublicChecked]);
 
 if ($stmt === NULL) {
 	header("location: ./../create_list.php?error=none");
