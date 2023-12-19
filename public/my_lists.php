58410e264cdb78f75b2eacd4a2a5e131440986b9
Kacper <kacperkapitrebacz@gmail.com>
2023-12-19 22:28:04 +0100

    FIX: Fixed a bug with session_start on user-related pages.

--- public/my_lists.php
+++ public/my_lists.php
@@ -1,14 +1,12 @@
 <?php
-if (!isset($_SESSION["userId"])) header("location: ./../index.php");
-
 require_once "./../vendor/autoload.php";
 require_once "./include/header.inc.php";
+
+if (!isset($_SESSION["userId"])) header("location: ./../index.php");
 
 $userId = $_SESSION["userId"];
 
 $resultLists = dbQuery("SELECT lists.list_id, lists.list_name, lists.is_public FROM lists WHERE lists.user_id = (SELECT users.user_id FROM users WHERE users.username = ?)", [$userId]);
-
-var_dump($resultLists);
 
 $template = $twig->load("my_lists.twig");
 print($template->render([
--- public/new_item.php
+++ public/new_item.php
@@ -1,8 +1,8 @@
 <?php
-if (!isset($_SESSION["userId"])) header("location: ./../index.php");
-
 require_once "./../vendor/autoload.php";
 require_once "./include/header.inc.php";
+
+if (!isset($_SESSION["userId"])) header("location: ./../index.php");
 
 $template = $twig->load("new_item.twig");
 print($template->render([
