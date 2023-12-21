61bc69e5e537e7a597235f412f71f32f307fb3a3
Kacper <kacperkapitrebacz@gmail.com>
2023-12-21 23:49:32 +0100

    FIX: list.php displays list information

--- public/list.php
+++ public/list.php
@@ -2,15 +2,16 @@
 require_once "./../vendor/autoload.php";
 require_once "./include/header.inc.php";
 
-if (isset($_GET["submit"]) && $_SERVER["REQUEST_METHOD"] == "GET") 
+if ($_SERVER["REQUEST_METHOD"] == "GET") 
 {
-	$getListName     = $_GET["list_name"];
-	$getListIsPublic = $_GET["is_public"];
+	$getListId = $_GET["list_id"];
+
+	$dbResult = dbQuery("SELECT lists.list_name, lists.is_public FROM lists WHERE lists.list_id = ?", [$getListId], true);
 
 	$template = $twig->load("list.twig");
 	print($template->render([
-		"isLogged"      => isset($_SESSION["userId"]),
-		'listName'      => $getListName,
-		'isListPrivate' => $getListIsPublic,
+		"isLogged" => isset($_SESSION["userId"]),
+		"listName" => $dbResult["list_name"],
+		"isPublic" => $dbResult["is_public"],
 	]));
 }
