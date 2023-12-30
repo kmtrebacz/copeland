35309260bad0226b3d56031118b999cc14d0ca4d
Kacper <kacperkapitrebacz@gmail.com>
2023-12-30 23:13:39 +0100

    FEAT: Add a user check before adding an item to the list

--- public/include/add_item_to_list.inc.php
+++ public/include/add_item_to_list.inc.php
@@ -4,6 +4,14 @@ require_once "./db_connection.inc.php";
 session_start();
 $db = dbConnect();
 
+function checkUser($listId): bool
+{
+     $dbResult = dbQuery("SELECT users.username FROM lists JOIN users ON users.user_id = lists.user_id WHERE lists.list_id = ? LIMIT 1;", [$listId], true);
+
+     if ($dbResult["username"] == $_SESSION["userId"]) return true;
+     else return false;
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "GET") 
 {
 	$listsId = [];
@@ -25,8 +33,11 @@ if ($_SERVER["REQUEST_METHOD"] == "GET")
 	//var_dump($listsId);
 
 	foreach ($listsId as $key => $value) 
-	{
-		$result = dbQuery("INSERT INTO list_items(list_id, item_id) VALUES (?, ?)", [$value, $itemId]);
+     {
+          if (checkUser($value)) 
+          {
+               $result = dbQuery("INSERT INTO list_items(list_id, item_id) VALUES (?, ?)", [$value, $itemId]);
+          }
 	}
 
 	header("location: ./../index.php?error=none");
